1f4a37c80ecb79174ad1c4563cdf0e64844dead6
Alan Harper <alan@aussiegeek.net>
2014-11-19 09:51:46 +1100

    Make it so

---
+++ README.md
@@ -0,0 +1,3 @@
+# Statsd Middleware for Negroni
+
+Small middleware that reports http status codes, and request timings to statds. Uses [http://github.com/cactus/go-statsd-client/statsd](http://github.com/cactus/go-statsd-client/statsd)
---
+++ middleware.go
@@ -0,0 +1,35 @@
+package negronistatsd
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/cactus/go-statsd-client/statsd"
+	"github.com/codegangsta/negroni"
+)
+
+// Middleware is a middleware handler that logs the request as it goes in and the response as it goes out.
+type Middleware struct {
+	StatsdClient *statsd.Client
+	// Name is the name of the application as recorded in latency metrics
+}
+
+// NewMiddleware returns a new *Middleware, yay!
+func NewMiddleware(statsd *statsd.Client) *Middleware {
+	return &Middleware{StatsdClient: statsd}
+}
+
+func (s *Middleware) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	start := time.Now()
+
+	next(rw, r)
+
+	// latency in milliseconds
+	latency := int64(time.Since(start) / 1000 / 1000)
+	res := rw.(negroni.ResponseWriter)
+
+	s.StatsdClient.Inc("requests."+strconv.Itoa(res.Status()), 1, 1.0)
+	s.StatsdClient.Timing("request_time", latency, 1.0)
+
+}
